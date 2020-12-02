b14028e941dc48972b90e68eed24bf02c21be0e3
skylarwang9 <63870999+skylarwang9@users.noreply.github.com>
2020-12-02 22:14:34 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  function direct(){
+	window.location.href = "sharedreams.html";
+  };
+  $('body').click(function(){
+	direct();
+  });
+});
---
+++ js/share.js
@@ -0,0 +1,18 @@
+const realFileBtn = document.getElementById("fileupload");
+const customBtn = document.getElementById("imgupload");
+const customTxt = document.getElementById("imgtext");
+
+customBtn.addEventListener("click", function() {
+  realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function() {
+  if (realFileBtn.value) {
+    customTxt.innerHTML = realFileBtn.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    customTxt.innerHTML = "No file chosen, yet.";
+  }
+});
+
